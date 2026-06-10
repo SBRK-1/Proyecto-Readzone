770c7a96cde3bf138f6a8fab0bc24dc33c13f0fd
Bryan <Bryan@gmail.com>
2026-06-10 12:31:59 -0600

    canbios en la conexion

--- conexion.js
+++ conexion.js
@@ -4,7 +4,7 @@ const pool = mysql.createConnection({
     host: 'acela.proxy.rlwy.net',              
     user: 'root',                   
     password: 'CUfMwashpeeRltZckCavAYvzQpWPkaPa', // La contraseña de tu MySQL Workbench
-    database: 'readzone',       // Tu base de datos de Workbench
+    database: 'railway',       // Tu base de datos de Workbench
     port: 27816,                     
     waitForConnections: true,
     connectionLimit: 10              
