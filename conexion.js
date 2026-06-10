a0ab7f7b19f058d94616c08ad124514f4fc7f47c
Bryan <Bryan@gmail.com>
2026-06-10 12:20:03 -0600

    cambios en conexio

--- conexion.js
+++ conexion.js
@@ -1,13 +1,12 @@
-const mysql = require('mysql2/promise'); // Conector en versión de Promesas
+const mysql = require('mysql2'); // Conector en versión de Promesas
 
-const pool = mysql.createPool({
-    host: 'localhost',              
+const pool = mysql.createConnection({
+    host: 'acela.proxy.rlwy.net',              
     user: 'root',                   
-    password: 'Redmi10#', // La contraseña de tu MySQL Workbench
+    password: 'CUfMwashpeeRltZckCavAYvzQpWPkaPa', // La contraseña de tu MySQL Workbench
     database: 'readzone',       // Tu base de datos de Workbench
-    port: 3306,                     
+    port: 27816,                     
     waitForConnections: true,
     connectionLimit: 10              
 });
 
-module.exports = pool;
